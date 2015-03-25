e6755373fcd9670225f2f7fb5171772c1fe468cc
Kaspar Hübener <kaspar.huebener@wooga.net>
2015-03-25 21:04:45 +0100

    added header functionality temporarily

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	resizeDiv();
+});
+
+window.onresize = function(event) {
+	resizeDiv();
+}
+
+function resizeDiv() {
+	vpw = $(window).width();
+	vph = $(window).height();
+	$('#intro').css({'height': vph + 'px'});
+	$('.gallery').css({'height': vph + 'px'});
+}
+
+
+$(document).scroll(function() {
+  //Show element after user scrolls 800px
+  var y = $(this).scrollTop();
+  if (y > 500) {
+    $('header.sidebar').addClass("visible");
+  } else {
+    $('header.sidebar').removeClass("visible");
+  }
+});
