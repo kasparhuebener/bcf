4dda24df43abf851067521e9aa10b1131dcc5502
Kaspar Hübener <kaspar.huebener@gmail.com>
2015-04-07 12:28:38 +0200

    cleaned up JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,76 @@
-$(document).ready(function(){
-  /* call resize function */
-  resizeElements();
+window.onload = function(event) {
+  /* call slideshow function */
+  toggleSlideshow();
+};
+window.onresize = function(event) {
+  toggleSlideshow();
+  initSkrollr();
+}
 
+/* based on scrolling, show menu bar if not on mobile */
+$(document).scroll(function() {
+  toggleHeader();
+});
+$(document).bind('touchmove',function(e) {
+  toggleHeader();
+});
+
+$(document).ready(function(){
+  initModals();
+  initMobileMenu();
+  initSkrollr();
+});
+
+function checkMaxWidth() {
+  return window.matchMedia("only screen and (min-width: 768px)").matches;
+}
+
+function initSkrollr() {
+  /* if max-width unlike touch device, init skrollr */
+    /* initialize skrollr */
+  if (checkMaxWidth()) {
+      var s = skrollr.init({
+        forceHeight: false
+      });
+
+      /* initialize skrollr menu */
+      //The options (second parameter) are all optional. The values shown are the default values.
+      skrollr.menu.init(s, {
+          //skrollr will smoothly animate to the new position using `animateTo`.
+          animate: true,
+          //The easing function to use.
+          easing: 'sqrt',
+          //Multiply your data-[offset] values so they match those set in skrollr.init
+          scale: 2,
+          //How long the animation should take in ms.
+          duration: function(currentTop, targetTop) {
+              //By default, the duration is hardcoded at 500ms.
+              //return 700;
+
+              //But you could calculate a value based on the current scroll position (`currentTop`) and the target scroll position (`targetTop`).
+              //return Math.abs(currentTop - targetTop) * 1;
+          },
+          //If you pass a handleLink function you'll disable `data-menu-top` and `data-menu-offset`.
+          //You are in control where skrollr will scroll to. You get the clicked link as a parameter and are expected to return a number.
+          //handleLink: function(link) {
+          //    return 400;//Hardcoding 400 doesn't make much sense.
+          //},
+          //By default skrollr-menu will only react to links whose href attribute contains a hash and nothing more, e.g. `href="#foo"`.
+          //If you enable `complexLinks`, skrollr-menu also reacts to absolute and relative URLs which have a hash part.
+          //The following will all work (if the user is on the correct page):
+          //http://example.com/currentPage/#foo
+          //http://example.com/currentDir/currentPage.html?foo=bar#foo
+          ///?foo=bar#foo
+          complexLinks: false,
+          //This event is triggered right before we jump/animate to a new hash.
+          change: function(newHash, newTopPosition) {
+              //Do stuff
+          }
+      });
+  }
+}
+
+function initModals() {
   /* initialize modals */
   $('#definition-btn').click(function() {
     $('#definition-modal')
@@ -48,7 +117,9 @@ $(document).ready(function(){
       .modal('show');
     ga('send', 'event', 'button', 'click', this.id);
   });
+}
 
+function initMobileMenu() {
   /* initialize mobile menu */
   $('#mobile-menu-btn').click(function() {
     $('#mobile-menu')
@@ -61,59 +132,9 @@ $(document).ready(function(){
       .sidebar('hide')
     ;
   });
-
-  /* if modernizr does not recognize touch device, init skrollr */
-    /* initialize skrollr */
-  if (!Modernizr.touch) {
-      var s = skrollr.init({
-        forceHeight: false
-      });
-
-      /* initialize skrollr menu */
-      //The options (second parameter) are all optional. The values shown are the default values.
-      skrollr.menu.init(s, {
-          //skrollr will smoothly animate to the new position using `animateTo`.
-          animate: true,
-          //The easing function to use.
-          easing: 'sqrt',
-          //Multiply your data-[offset] values so they match those set in skrollr.init
-          scale: 2,
-          //How long the animation should take in ms.
-          duration: function(currentTop, targetTop) {
-              //By default, the duration is hardcoded at 500ms.
-              //return 700;
-
-              //But you could calculate a value based on the current scroll position (`currentTop`) and the target scroll position (`targetTop`).
-              //return Math.abs(currentTop - targetTop) * 1;
-          },
-          //If you pass a handleLink function you'll disable `data-menu-top` and `data-menu-offset`.
-          //You are in control where skrollr will scroll to. You get the clicked link as a parameter and are expected to return a number.
-          //handleLink: function(link) {
-          //    return 400;//Hardcoding 400 doesn't make much sense.
-          //},
-          //By default skrollr-menu will only react to links whose href attribute contains a hash and nothing more, e.g. `href="#foo"`.
-          //If you enable `complexLinks`, skrollr-menu also reacts to absolute and relative URLs which have a hash part.
-          //The following will all work (if the user is on the correct page):
-          //http://example.com/currentPage/#foo
-          //http://example.com/currentDir/currentPage.html?foo=bar#foo
-          ///?foo=bar#foo
-          complexLinks: false,
-          //This event is triggered right before we jump/animate to a new hash.
-          change: function(newHash, newTopPosition) {
-              //Do stuff
-          }
-      });
-  }
-
-});
-
-window.onresize = function(event) {
-	resizeElements();
 }
 
-function resizeElements() {
-	vpw = $(window).width();
-	vph = $(window).height();
+function toggleSlideshow() {
 
   /* initialize flickity slideshow*/
   flkty = $('#festival').flickity({
@@ -125,8 +146,7 @@ function resizeElements() {
     imagesLoaded: true
   });
 
-  if (vpw >= 768) {
-
+  if (checkMaxWidth()) {
     /* slide show only makes sense on not mobile screens */
     $('#festival').toggleClass('noscript');
     $('#festival').flickity('resize');
@@ -142,24 +162,14 @@ function resizeElements() {
 }
 
 function toggleHeader() {
-    // Version 1: set relative to element
-    // var skrollrInstance = skrollr.get();
-    // var offsetFestival =skrollrInstance.relativeToAbsolute(document.getElementById('#festival'), 'top', 'top');
+    /* TODO: fix header on iPad.
+    ** register for the right touch events
+    ** and check window.innerHeight
+    */ 
+  if (checkMaxWidth()) {
+    var distance = $(this).scrollTop(); 
     
-    // Version 2: hardcoded
-    // Show element after user scrolls 500px
-    // but only for not mobile devices.
-  if (typeof vpw != 'undefined' && vpw >= 768) {
-    /* detect scrolled distance differently for touch devices */
-    var distance;
-    if (Modernizr.touch) {
-      distance = window.pageYOffset;
-    }
-    else {
-      distance = $(this).scrollTop(); 
-    }
-    // check for y > 500
-    if (distance > vph-200) {
+    if (distance > $(window).height()-200) {
       $('#header')
         .sidebar('setting','dimPage', false)
         .sidebar('setting','closable', false)
@@ -170,11 +180,3 @@ function toggleHeader() {
     }
   }
 }
-
-/* based on scrolling, show menu bar if not on mobile */
-$(document).scroll(function() {
-  toggleHeader();
-});
-$(document).bind('touchmove',function(e) {
-  toggleHeader();
-});
